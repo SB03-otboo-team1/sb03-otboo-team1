fea4ae9dd05fa6fdfdcb77bf64380fddf1a783c7
NINE <99791512+NINE-J@users.noreply.github.com>
2025-09-11 20:48:53 +0900

    chore: GlobalExceptionHandler - GREEN

--- src/main/java/com/onepiece/otboo/domain/user/exception/UserException.java
+++ src/main/java/com/onepiece/otboo/domain/user/exception/UserException.java
@@ -1,5 +1,16 @@
 package com.onepiece.otboo.domain.user.exception;
 
-public class UserException {
+import com.onepiece.otboo.global.exception.CustomException;
+import com.onepiece.otboo.global.exception.ErrorCode;
+import java.util.Map;
 
+public class UserException extends CustomException {
+
+    public UserException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+
+    public UserException(ErrorCode errorCode, Map<String, Object> details) {
+        super(errorCode, details);
+    }
 }
