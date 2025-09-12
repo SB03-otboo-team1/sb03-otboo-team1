788a1b630661e1931006aed133c514b1dceae9ec
JinsolPark <jinsol8k@gmail.com>
2025-09-12 11:35:46 +0900

    chore: 의상 및 의상 추천 도메인 패키지 구성

---
+++ src/main/java/com/onepiece/otboo/domain/clothes/api/ClothesApi.java
@@ -0,0 +1,5 @@
+package com.onepiece.otboo.domain.clothes.api;
+
+public class ClothesApi {
+
+}
---
+++ src/main/java/com/onepiece/otboo/domain/clothes/repository/ClothesRepository.java
@@ -0,0 +1,5 @@
+package com.onepiece.otboo.domain.clothes.repository;
+
+public class ClothesRepository {
+
+}
