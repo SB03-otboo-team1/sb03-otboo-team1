4fa0c16e04c0b48dc87ed7d98be5ba68f035f480
JinsolPark <jinsol8k@gmail.com>
2025-09-12 16:40:16 +0900

    feat: 의상 관련 Entity, Dto 추가, JPA 연동

--- src/main/java/com/onepiece/otboo/domain/clothes/entity/Clothes.java
+++ src/main/java/com/onepiece/otboo/domain/clothes/entity/Clothes.java
@@ -1,5 +1,26 @@
 package com.onepiece.otboo.domain.clothes.entity;
 
-public class Clothes {
+import com.onepiece.otboo.global.base.BaseUpdatableEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name = "clothes")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Clothes extends BaseUpdatableEntity {
+
+  @Column(nullable = false, length = 250)
+  private String name;
+
+  @Column(nullable = false)
+  private ClothesType type;
+
+  @Column(name = "image_url", nullable = true)
+  private String imageUrl;
 
 }
--- src/main/java/com/onepiece/otboo/domain/clothes/repository/ClothesRepository.java
+++ src/main/java/com/onepiece/otboo/domain/clothes/repository/ClothesRepository.java
@@ -1,5 +1,11 @@
 package com.onepiece.otboo.domain.clothes.repository;
 
-public class ClothesRepository {
+import com.onepiece.otboo.domain.clothes.entity.Clothes;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClothesRepository extends JpaRepository<Clothes, UUID> {
 
 }
