05bf3795e27b3665ad9de274cb0a80ae2d888d17
adjoon1 <minj97@naver.com>
2025-09-16 05:41:48 +0900

    feat: FollowData DTO 추가

--- src/main/java/com/onepiece/otboo/domain/follow/dto/data/FollowData.java
+++ src/main/java/com/onepiece/otboo/domain/follow/dto/data/FollowData.java
@@ -1,5 +1,16 @@
 package com.onepiece.otboo.domain.follow.dto.data;
 
-public class Data {
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
+@Builder
+@AllArgsConstructor
+
+public class FollowData {
+    private UUID id;
+    private UUID followerId;
+    private UUID followingId;
 }
