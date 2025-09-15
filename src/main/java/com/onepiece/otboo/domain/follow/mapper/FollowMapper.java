321d18faab2b13c1d3472f12df107e6ec0843a5e
adjoon1 <minj97@naver.com>
2025-09-16 05:44:30 +0900

    feat: FollowMapper 구현

--- src/main/java/com/onepiece/otboo/domain/follow/mapper/FollowMapper.java
+++ src/main/java/com/onepiece/otboo/domain/follow/mapper/FollowMapper.java
@@ -1,5 +1,20 @@
 package com.onepiece.otboo.domain.follow.mapper;
 
-public class UserMapper {
+import com.onepiece.otboo.domain.follow.dto.data.FollowData;
+import com.onepiece.otboo.domain.follow.dto.response.FollowResponse;
+import com.onepiece.otboo.domain.follow.entity.Follow;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface FollowMapper {
+
+    @Mapping(source = "follower.id", target = "followerId")
+    @Mapping(source = "following.id", target = "followingId")
+    FollowResponse toResponse(Follow follow);
+
+    @Mapping(source = "follower.id", target = "followerId")
+    @Mapping(source = "following.id", target = "followingId")
+    FollowData toData(Follow follow);
 
 }
