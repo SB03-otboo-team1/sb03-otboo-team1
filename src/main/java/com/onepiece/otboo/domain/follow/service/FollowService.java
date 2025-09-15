9724549b6fb638baa9cf37bd17680a116460591b
adjoon1 <minj97@naver.com>
2025-09-16 05:45:00 +0900

    feat: FollowService 인터페이스 정의

--- src/main/java/com/onepiece/otboo/domain/follow/service/FollowService.java
+++ src/main/java/com/onepiece/otboo/domain/follow/service/FollowService.java
@@ -1,5 +1,19 @@
 package com.onepiece.otboo.domain.follow.service;
 
-public class UserService {
+import com.onepiece.otboo.domain.follow.dto.request.FollowRequest;
+import com.onepiece.otboo.domain.follow.dto.response.FollowResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface FollowService {
+
+    FollowResponse createFollow(FollowRequest request);
+
+    List<FollowResponse> getFollowers(UUID userId);
+
+    List<FollowResponse> getFollowings(UUID userId);
+
+    void deleteFollow(FollowRequest request);
 
 }
