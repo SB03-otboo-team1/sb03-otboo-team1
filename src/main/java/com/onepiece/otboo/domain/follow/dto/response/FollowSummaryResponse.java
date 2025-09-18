d5f693f824158fc9636d6a5f4f71457e8e0796df
adjoon1 <minj97@naver.com>
2025-09-18 13:18:37 +0900

    fix: FollowSummaryResponse 직렬화 시 isFollowing 필드 정상 노출

--- src/main/java/com/onepiece/otboo/domain/follow/dto/response/FollowSummaryResponse.java
+++ src/main/java/com/onepiece/otboo/domain/follow/dto/response/FollowSummaryResponse.java
@@ -1,4 +1,23 @@
 package com.onepiece.otboo.domain.follow.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FollowSummaryResponse {
+
+    private UUID userId;
+    private long followerCount;
+    private long followingCount;
+    
+    @JsonProperty("isFollowing")
+    private boolean isFollowing;
 }
