9e6417bf39e87449bf02b2537631e7ee8bad5c75
NINE <99791512+NINE-J@users.noreply.github.com>
2025-09-19 18:19:21 +0900

    chore: 통합 테스트 소스셋 구성
    - integrationTest 생성
    - build.gradle 소스셋 설정
    - test-integration 프로필 추가

---
+++ src/integrationTest/java/com/onepiece/otboo/OtbooApplicationIntegrationTests.java
@@ -0,0 +1,13 @@
+package com.onepiece.otboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtbooApplicationIntegrationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
