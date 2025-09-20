1ca6dd0fb79dbd988ef5fcf54d49c9a1386352f5
NINE <99791512+NINE-J@users.noreply.github.com>
2025-09-20 15:36:03 +0900

    chore: TestSecurityConfig 수정
    - 미인증 객체에 대한 처리
    - 엔트리 포인트 적용
    - 향후 인증 필요한 엔드포인트 추가 가능

--- src/test/java/com/onepiece/otboo/infra/security/config/TestSecurityConfig.java
+++ src/test/java/com/onepiece/otboo/infra/security/config/TestSecurityConfig.java
@@ -6,11 +6,13 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Slf4j
 @TestConfiguration
@@ -22,7 +24,16 @@ public class TestSecurityConfig {
     SecurityFilterChain testFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+            .authorizeHttpRequests(auth -> auth
+                // OPTION: 향후 인증 테스트가 필요하다면 엔드포인트 추가
+                .anyRequest().permitAll()
+            )
+            .anonymous(AbstractHttpConfigurer::disable)
+            .exceptionHandling(eh -> eh
+                .authenticationEntryPoint(
+                    new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            );
+
         return http.build();
     }
 
