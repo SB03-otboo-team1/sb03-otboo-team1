d8804a17fa18a52ed0d994ab761e20a16b8f9736
Dw-real <gksehddn8@naver.com>
2025-09-21 14:14:07 +0900

    test: WeatherController 날씨 정보 조회 테스트 추가

--- src/test/java/com/onepiece/otboo/domain/weather/controller/WeatherControllerTest.java
+++ src/test/java/com/onepiece/otboo/domain/weather/controller/WeatherControllerTest.java
@@ -9,9 +9,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.onepiece.otboo.domain.location.service.LocationService;
 import com.onepiece.otboo.domain.weather.dto.data.WeatherAPILocation;
+import com.onepiece.otboo.domain.weather.dto.response.HumidityDto;
+import com.onepiece.otboo.domain.weather.dto.response.PrecipitationDto;
+import com.onepiece.otboo.domain.weather.dto.response.TemperatureDto;
+import com.onepiece.otboo.domain.weather.dto.response.WeatherDto;
+import com.onepiece.otboo.domain.weather.dto.response.WindSpeedDto;
+import com.onepiece.otboo.domain.weather.enums.PrecipitationType;
+import com.onepiece.otboo.domain.weather.enums.SkyStatus;
+import com.onepiece.otboo.domain.weather.enums.WindSpeedWord;
 import com.onepiece.otboo.domain.weather.service.WeatherService;
 import com.onepiece.otboo.global.config.JpaConfig;
+import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,22 +48,29 @@ class WeatherControllerTest {
     @MockitoBean
     private WeatherService weatherService;
 
-    @Test
-    void 위치_정보_조회_성공시_200을_반환한다() throws Exception {
+    private double longitude = 126.8054724084087;
+    private double latitude = 37.43751196107601;
+    private WeatherAPILocation location;
 
-        // given
-        double longitude = 126.8054724084087;
-        double latitude = 37.43751196107601;
+    @BeforeEach
+    void setUp() {
+        longitude = 126.8054724084087;
+        latitude = 37.43751196107601;
 
-        WeatherAPILocation weatherAPILocation = new WeatherAPILocation(
+        location = new WeatherAPILocation(
             37.4375,
             126.8054,
             57,
             124,
             List.of("경기도", "시흥시", "은행동")
         );
+    }
 
-        given(locationService.getLocation(anyDouble(), anyDouble())).willReturn(weatherAPILocation);
+    @Test
+    void 위치_정보_조회_성공시_200을_반환한다() throws Exception {
+
+        // given
+        given(locationService.getLocation(anyDouble(), anyDouble())).willReturn(location);
 
         // when
         ResultActions result = mockMvc.perform(get("/api/weathers/location")
@@ -66,5 +84,43 @@ class WeatherControllerTest {
             .andExpect(jsonPath("$.y").value(String.valueOf(124)))
             .andExpect(jsonPath("$.locationNames[0]").value("경기도"));
         verify(locationService).getLocation(longitude, latitude);
+    }
+
+    @Test
+    void 날씨_정보_조회_성공시_200을_반환한다() throws Exception {
+
+        // given
+        UUID weatherId = UUID.randomUUID();
+
+        WeatherDto weatherDto = WeatherDto.builder()
+            .id(weatherId)
+            .forecastedAt(ZonedDateTime.parse("2025-09-20T05:00:00+09:00"))
+            .forecastAt(ZonedDateTime.parse("2025-09-21T14:00:00+09:00"))
+            .location(location)
+            .skyStatus(SkyStatus.CLOUDY)
+            .precipitation(new PrecipitationDto(PrecipitationType.NONE, 0.0, 30.0))
+            .humidity(new HumidityDto(55.0, -5.0))
+            .temperature(new TemperatureDto(22.0, -2.0, 18.0, 24.0))
+            .windSpeed(new WindSpeedDto(1.0, WindSpeedWord.WEAK))
+            .build();
+
+        given(weatherService.getWeather(anyDouble(), anyDouble()))
+            .willReturn(List.of(weatherDto));
+
+        // when
+        ResultActions result = mockMvc.perform(get("/api/weathers")
+            .param("longitude", String.valueOf(longitude))
+            .param("latitude", String.valueOf(latitude))
+            .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        result.andExpect(status().isOk())
+            .andExpect(jsonPath("$[0].id").value(weatherId.toString()))
+            .andExpect(jsonPath("$[0].skyStatus").value("CLOUDY"))
+            .andExpect(jsonPath("$[0].precipitation.type").value("NONE"))
+            .andExpect(jsonPath("$[0].humidity.current").value("55.0"))
+            .andExpect(jsonPath("$[0].temperature.current").value("22.0"))
+            .andExpect(jsonPath("$[0].windSpeed.asWord").value("WEAK"));
+        verify(weatherService).getWeather(longitude, latitude);
     }
 }
