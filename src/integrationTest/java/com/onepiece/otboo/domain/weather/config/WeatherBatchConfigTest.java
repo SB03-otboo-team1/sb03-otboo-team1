53091b14c4f28909d5b3cb917afde801c018b0d8
NINE <99791512+NINE-J@users.noreply.github.com>
2025-09-22 11:06:58 +0900

    fix: 통합 테스트 security 빈 로드 점검

--- src/integrationTest/java/com/onepiece/otboo/domain/weather/config/WeatherBatchConfigTest.java
+++ src/integrationTest/java/com/onepiece/otboo/domain/weather/config/WeatherBatchConfigTest.java
@@ -23,7 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("dev")
+@ActiveProfiles("test-integration")
 @SpringBatchTest
 @SpringBootTest
 class WeatherBatchConfigTest {
