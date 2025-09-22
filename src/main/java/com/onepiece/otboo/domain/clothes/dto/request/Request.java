cf86651bb2f1f010bf56ef525331c181c89769ff
JinsolPark <jinsol8k@gmail.com>
2025-09-22 17:37:18 +0900

    feat: 옷 목록 조회 기능 구현(red)

--- src/main/java/com/onepiece/otboo/domain/clothes/dto/request/Request.java
+++
@@ -1,5 +0,0 @@
-package com.onepiece.otboo.domain.clothes.dto.request;
-
-public class Request {
-
-}
--- src/main/java/com/onepiece/otboo/domain/clothes/entity/Attributes.java
+++
@@ -1,5 +0,0 @@
-package com.onepiece.otboo.domain.clothes.entity;
-
-public class Attributes {
-
-}
