e41afa0de46018aeef29d6058ffe3976007ac81b
adjoon1 <minj97@naver.com>
2025-09-23 10:32:46 +0900

    feat: 팔로잉 목록 응답 DTO 추가 (닉네임, 프로필 포함)

--- src/main/java/com/onepiece/otboo/domain/follow/dto/response/FollowingResponse.java
+++ src/main/java/com/onepiece/otboo/domain/follow/dto/response/FollowingResponse.java
@@ -1,4 +1,18 @@
 package com.onepiece.otboo.domain.follow.dto.response;
 
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class FollowingResponse {
+    private UUID id;
+    private UUID followingId;
+    private String nickname;
+    private String profileImage;
+    private Instant createdAt;
 }
