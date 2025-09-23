6b312309ff9e90103254459820d1dc225055f9e3
adjoon1 <minj97@naver.com>
2025-09-24 03:51:39 +0900

    refactor: FollowService 및 Controller에 팔로워/팔로잉 조회 기능 구현

--- src/main/java/com/onepiece/otboo/domain/follow/service/FollowService.java
+++ src/main/java/com/onepiece/otboo/domain/follow/service/FollowService.java
@@ -4,17 +4,33 @@ import com.onepiece.otboo.domain.follow.dto.request.FollowRequest;
 import com.onepiece.otboo.domain.follow.dto.response.FollowResponse;
 import com.onepiece.otboo.domain.follow.dto.response.FollowSummaryResponse;
 import com.onepiece.otboo.domain.follow.dto.response.FollowingResponse;
+import com.onepiece.otboo.global.dto.response.CursorPageResponseDto;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface FollowService {
 
     FollowResponse createFollow(FollowRequest request);
 
-    List<FollowResponse> getFollowers(UUID userId);
+    CursorPageResponseDto<FollowResponse> getFollowers(
+        UUID followeeId,
+        String cursor,
+        UUID idAfter,
+        int limit,
+        String nameLike,
+        String sortBy,
+        String sortDirection
+    );
 
-    List<FollowingResponse> getFollowings(UUID userId);
+    CursorPageResponseDto<FollowingResponse> getFollowings(
+        UUID followerId,
+        String cursor,
+        UUID idAfter,
+        int limit,
+        String nameLike,
+        String sortBy,
+        String sortDirection
+    );
 
     void deleteFollow(FollowRequest request);
 
