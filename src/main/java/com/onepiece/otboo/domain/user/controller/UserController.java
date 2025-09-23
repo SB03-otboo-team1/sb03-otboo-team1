7f68357551f0298c5670c5c994ce49ceb640741b
NINE <99791512+NINE-J@users.noreply.github.com>
2025-09-23 14:31:12 +0900

    test: 어드민 초기화 및 권한 수정, 잠금 관리 통합 테스트

--- src/main/java/com/onepiece/otboo/domain/user/controller/UserController.java
+++ src/main/java/com/onepiece/otboo/domain/user/controller/UserController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +45,7 @@ public class UserController implements UserApi {
     }
 
     @Override
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("{userId}/role")
     public ResponseEntity<Void> changeRole(
         @PathVariable("userId") String userId,
@@ -57,6 +59,7 @@ public class UserController implements UserApi {
     }
 
     @Override
+    @PreAuthorize("hasRole('ADMIN')")
     @PatchMapping("/{userId}/lock")
     public ResponseEntity<UserDto> updateUserLock(@PathVariable("userId") String userId,
         @Valid @RequestBody UserLockUpdateRequest request) {
