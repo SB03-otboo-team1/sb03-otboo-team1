ac7aff6b5ec1f93626dd8e611ef894e51bdd59c4
Dw-real <gksehddn8@naver.com>
2025-09-24 11:36:18 +0900

    add: 프로필 업데이트를 위한 ProfileUpdateRequest 추가

---
+++ src/main/java/com/onepiece/otboo/domain/profile/dto/request/ProfileUpdateRequest.java
@@ -0,0 +1,27 @@
+package com.onepiece.otboo.domain.profile.dto.request;
+
+import com.onepiece.otboo.domain.profile.enums.Gender;
+import com.onepiece.otboo.domain.weather.dto.data.WeatherAPILocation;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDate;
+import lombok.Builder;
+
+@Builder
+public record ProfileUpdateRequest(
+    @NotBlank
+    String name,
+
+    Gender gender,
+
+    LocalDate birthDate,
+
+    WeatherAPILocation location,
+
+    @Min(value = 1, message = "온도 민감도는 최소 1입니다.")
+    @Max(value = 5, message = "온도 민감도는 최대 5입니다.")
+    Integer temperatureSensitivity
+) {
+
+}
