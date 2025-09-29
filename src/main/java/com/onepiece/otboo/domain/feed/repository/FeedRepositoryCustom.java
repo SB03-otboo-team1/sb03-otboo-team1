39bd4f3b11cb1b7e8ef8d8eeb80d27cb54196212
noonsong0208 <tmdwls7795@gmail.com>
2025-09-29 18:05:29 +0900

    feat: 중복 로직 제거

---
+++ src/main/java/com/onepiece/otboo/domain/feed/repository/FeedRepositoryCustom.java
@@ -0,0 +1,11 @@
+//package com.onepiece.otboo.domain.feed.repository;
+//
+//import com.onepiece.otboo.domain.feed.dto.request.FeedListRequest;
+//import com.onepiece.otboo.domain.feed.entity.Feed;
+//
+//import java.util.List;
+//
+//public interface FeedRepositoryCustom {
+//    List<Feed> findSlice(FeedListRequest req);
+//    long countAll(FeedListRequest req);
+//}
