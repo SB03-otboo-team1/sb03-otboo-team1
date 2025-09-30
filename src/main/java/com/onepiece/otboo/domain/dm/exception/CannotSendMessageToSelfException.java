e896998f4f8d0cd332ce597b976be0e240d60e80
adjoon1 <minj97@naver.com>
2025-09-30 16:34:45 +0900

    feat: 자기 자신에게 DM 전송 예외 추가

--- src/main/java/com/onepiece/otboo/domain/dm/exception/CannotSendMessageToSelfException.java
+++ src/main/java/com/onepiece/otboo/domain/dm/exception/CannotSendMessageToSelfException.java
@@ -1,7 +1,11 @@
 package com.onepiece.otboo.domain.dm.exception;
 
-public class CannotSendMessageToSelfException extends RuntimeException {
-  public CannotSendMessageToSelfException(String message) {
-    super(message);
-  }
+import com.onepiece.otboo.global.exception.ErrorCode;
+import com.onepiece.otboo.global.exception.GlobalException;
+
+public class CannotSendMessageToSelfException extends GlobalException {
+
+    public CannotSendMessageToSelfException() {
+        super(ErrorCode.CANNOT_SEND_MESSAGE_TO_SELF);
+    }
 }
