affa81dd0ad18b7080b5264b2445719eeab7ce61
adjoon1 <minj97@naver.com>
2025-09-30 16:35:18 +0900

    feat: DirectMessageService 인터페이스 추가

--- src/main/java/com/onepiece/otboo/domain/dm/service/DirectMessageService.java
+++ src/main/java/com/onepiece/otboo/domain/dm/service/DirectMessageService.java
@@ -1,4 +1,17 @@
 package com.onepiece.otboo.domain.dm.service;
 
-public class DirectMessageService {
+import com.onepiece.otboo.domain.dm.dto.request.DirectMessageRequest;
+import com.onepiece.otboo.domain.dm.dto.response.DirectMessageResponse;
+import java.util.List;
+import java.util.UUID;
+
+public interface DirectMessageService {
+
+    DirectMessageResponse createDirectMessage(DirectMessageRequest request);
+
+    DirectMessageResponse getDirectMessageById(UUID id);
+
+    List<DirectMessageResponse> getDirectMessages(UUID userId, String cursor, UUID idAfter,
+        int limit);
+
 }
