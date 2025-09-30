5e5209920d8e7102673dcf5530b20f6beec128c9
noonsong0208 <tmdwls7795@gmail.com>
2025-09-30 15:38:36 +0900

    refactor: 사용되지 않는 클래스 삭제

--- src/main/java/com/onepiece/otboo/domain/feed/dto/request/FeedListRequest.java
+++ src/main/java/com/onepiece/otboo/domain/feed/dto/request/FeedListRequest.java
@@ -1,20 +1,20 @@
-package com.onepiece.otboo.domain.feed.dto.request;
-
-
-
-import com.onepiece.otboo.domain.feed.enums.FeedSortBy;
-import com.onepiece.otboo.domain.feed.enums.SortDirection;
-
-import java.util.UUID;
-
-public record FeedListRequest(
-    String cursor,
-    UUID idAfter,
-    int limit,
-    FeedSortBy sortBy,
-    SortDirection sortDirection,
-    String keywordLike,
-    String skyStatusEqual,
-    String precipitationTypeEqual,
-    UUID authorIdEqual
-) { }
+//package com.onepiece.otboo.domain.feed.dto.request;
+//
+//
+//
+//import com.onepiece.otboo.domain.feed.enums.FeedSortBy;
+//import com.onepiece.otboo.global.enums.SortDirection;
+//
+//import java.util.UUID;
+//
+//public record FeedListRequest(
+//    String cursor,
+//    UUID idAfter,
+//    int limit,
+//    FeedSortBy sortBy,
+//    SortDirection sortDirection,
+//    String keywordLike,
+//    String skyStatusEqual,
+//    String precipitationTypeEqual,
+//    UUID authorIdEqual
+//) { }
