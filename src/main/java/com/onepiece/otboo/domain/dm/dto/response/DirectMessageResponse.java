ebfb590226b93f75a8572fe1602fb6af18c84f55
adjoon1 <minj97@naver.com>
2025-09-30 16:34:33 +0900

    feat: DirectMessageResponse DTO 추가

--- src/main/java/com/onepiece/otboo/domain/dm/dto/response/DirectMessageResponse.java
+++ src/main/java/com/onepiece/otboo/domain/dm/dto/response/DirectMessageResponse.java
@@ -1,5 +1,33 @@
 package com.onepiece.otboo.domain.dm.dto.response;
 
-public class DirectMessageResponseDto {
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DirectMessageResponse {
+
+    private UUID id;
+    private Instant createdAt;
+
+    private UserInfo sender;
+    private UserInfo receiver;
+
+    private String content;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UserInfo {
+
+        private UUID userId;
+        private String email;
+    }
 }
