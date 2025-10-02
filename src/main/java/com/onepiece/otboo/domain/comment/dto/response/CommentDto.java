6993ec1166f64410f6b5c2b805a10e809d3ee0b5
noonsong0208 <tmdwls7795@gmail.com>
2025-10-02 13:13:49 +0900

    feat: 응답 DTO 추가

---
+++ src/main/java/com/onepiece/otboo/domain/comment/dto/response/CommentDto.java
@@ -0,0 +1,15 @@
+package com.onepiece.otboo.domain.comment.dto.response;
+
+import com.onepiece.otboo.domain.feed.dto.response.AuthorDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.Instant;
+import java.util.UUID;
+
+@Schema(description = "피드 댓글 응답")
+public record CommentDto(
+    UUID id,
+    Instant createdAt,
+    UUID feedId,
+    AuthorDto author,
+    String content
+) { }
