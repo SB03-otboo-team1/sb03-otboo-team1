8dde2fd59ac735c450d2d816649dfde96faab1de
noonsong0208 <tmdwls7795@gmail.com>
2025-10-02 18:37:15 +0900

    fix: AuthorDto 작성자 정보 누락 수정, User↔Profile 연관 및 위임 게터 추가

--- src/main/java/com/onepiece/otboo/domain/comment/mapper/CommentMapper.java
+++ src/main/java/com/onepiece/otboo/domain/comment/mapper/CommentMapper.java
@@ -3,13 +3,11 @@ package com.onepiece.otboo.domain.comment.mapper;
 
 import com.onepiece.otboo.domain.comment.dto.response.CommentDto;
 import com.onepiece.otboo.domain.comment.entity.Comment;
-import com.onepiece.otboo.domain.feed.dto.response.AuthorDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
-
     @Mapping(target = "feedId", expression = "java(comment.getFeed().getId())")
     @Mapping(target = "author", expression =
         "java(new AuthorDto(comment.getAuthor().getId(), comment.getAuthor().getNickname(), comment.getAuthor().getProfileImage()))")
