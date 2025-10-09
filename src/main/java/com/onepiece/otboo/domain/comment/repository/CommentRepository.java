1728341acf63742e6434066c763df90f6c146539
noonsong0208 <tmdwls7795@gmail.com>
2025-10-10 05:19:50 +0900

    feat: 댓글 총 개수 조회 메서드 추가

--- src/main/java/com/onepiece/otboo/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/onepiece/otboo/domain/comment/repository/CommentRepository.java
@@ -4,5 +4,6 @@ import com.onepiece.otboo.domain.comment.entity.Comment;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository extends JpaRepository<Comment, UUID> {
+public interface CommentRepository extends JpaRepository<Comment, UUID>, CommentRepositoryCustom {
+    long countByFeed_Id(UUID feedId);
 }
