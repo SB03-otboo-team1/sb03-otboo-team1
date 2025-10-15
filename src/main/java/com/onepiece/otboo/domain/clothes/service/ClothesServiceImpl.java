bc4017df3b34d6e62a36819f33274f67e4525341
JinsolPark <jinsol8k@gmail.com>
2025-10-15 16:19:59 +0900

    fix: 의상 수정 시 의상 속성 초기화되는 오류 수정 (속성 업데이트 코드 추가)

--- src/main/java/com/onepiece/otboo/domain/clothes/service/ClothesServiceImpl.java
+++ src/main/java/com/onepiece/otboo/domain/clothes/service/ClothesServiceImpl.java
@@ -163,7 +163,7 @@ public class ClothesServiceImpl implements ClothesService {
             .imageUrl(imageUrl)
             .build();
 
-        clothesRepository.save(clothes);
+        Clothes savedClothes = clothesRepository.save(clothes);
 
         // Attributes 생성해서 저장
         List<ClothesAttributeDto> attrDto = request.attributes();
@@ -183,18 +183,18 @@ public class ClothesServiceImpl implements ClothesService {
                 }
             ).toList();
 
-        attributeRepository.saveAll(attributes);
+        List<ClothesAttributes> savedAttributes = attributeRepository.saveAll(attributes);
 
         // ClothesDto 생성용 ClothesAttributeWithDefDto 생성
         List<ClothesAttributeWithDefDto> clothesAttributeWithDefDto =
-            attributes.stream().map(a -> {
+            savedAttributes.stream().map(a -> {
                 ClothesAttributeDefs def = a.getDefinition();
                 String value = a.getOptionValue();
                 return clothesAttributeMapper.toAttributeWithDefDto(def, value);
             }).toList();
 
         // ClothesDto 반환
-        return clothesMapper.toDto(clothes, clothesAttributeWithDefDto, fileStorage);
+        return clothesMapper.toDto(savedClothes, clothesAttributeWithDefDto, fileStorage);
     }
 
     @Override
@@ -212,11 +212,41 @@ public class ClothesServiceImpl implements ClothesService {
             newImageUrl = fileStorage.uploadFile(CLOTHES_PREFIX, imageFile);
         }
 
+        List<ClothesAttributes> attributes = attributeRepository.findByClothesId(clothesId);
+
+        List<ClothesAttributes> newAttributes =
+            request.attributes().stream().map(
+                dto -> {
+                    ClothesAttributeDefs def = defRepository.findById(dto.definitionId())
+                        .orElseThrow(
+                            () -> ClothesAttributeDefNotFoundException.byId(dto.definitionId())
+                        );
+                    String value = dto.value();
+                    return ClothesAttributes.builder()
+                        .clothes(clothes)
+                        .definition(def)
+                        .optionValue(value)
+                        .build();
+                }
+            ).toList();
+
+        if (!newAttributes.isEmpty() && !newAttributes.equals(attributes)) {
+            attributeRepository.deleteAll(attributes);
+            attributes = attributeRepository.saveAll(newAttributes);
+        }
+
         clothes.update(newName, newType, newImageUrl);
 
         Clothes updatedClothes = clothesRepository.save(clothes);
 
-        return clothesMapper.toDto(updatedClothes, Collections.emptyList(), fileStorage);
+        List<ClothesAttributeWithDefDto> clothesAttributeWithDefDto =
+            attributes.stream().map(a -> {
+                ClothesAttributeDefs def = a.getDefinition();
+                String value = a.getOptionValue();
+                return clothesAttributeMapper.toAttributeWithDefDto(def, value);
+            }).toList();
+
+        return clothesMapper.toDto(updatedClothes, clothesAttributeWithDefDto, fileStorage);
     }
 
     @Override
