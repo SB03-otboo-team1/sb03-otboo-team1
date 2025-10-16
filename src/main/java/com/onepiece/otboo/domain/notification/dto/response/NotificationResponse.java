90a9e35b3424af4ee48c1aea8676b448db2a8ad4
adjoon1 <minj97@naver.com>
2025-10-16 15:23:27 +0900

    feat: NotificationResponse DTO 필드 및 구조 작성

--- src/main/java/com/onepiece/otboo/domain/notification/dto/response/NotificationResponse.java
+++ src/main/java/com/onepiece/otboo/domain/notification/dto/response/NotificationResponse.java
@@ -1,4 +1,19 @@
 package com.onepiece.otboo.domain.notification.dto.response;
 
+import com.onepiece.otboo.domain.notification.enums.Level;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class NotificationResponse {
+
+    private UUID id;
+    private UUID receiverId;
+    private String title;
+    private String content;
+    private Level level;
+    private Instant createdAt;
 }
